1a6e8809e51d7bcd95fcfab0891ca1a989750ab4
Ufuk Tugay <ufukkaantugay@gmail.com>
2021-10-23 19:47:27 +0200

    added index html file

--- DataGrabber.py
+++ DataGrabber.py
@@ -1,24 +1,26 @@
 import yfinance as yf
+import math
 
-company = "MSFT"
+company = "IMBBY"
+
+
+
 '''
 Returns the net cashflow of the given ticker symbol for the last few years. Net cashflow is calculated from cash from operations and the capital expenditure.
 '''
 def get_cashflow(ticker):
     ticker = yf.Ticker(company)
     ticker_cashflow = ticker.cashflow
-
     ticker_cashflow= ticker_cashflow.to_dict('index')
 
     cash_from_op = ticker_cashflow["Total Cash From Operating Activities"]
     cap_ex = ticker_cashflow["Capital Expenditures"]
-    print(cash_from_op, "\n")
+    print(f"{company} Free Cashflow: ",cash_from_op)
     net_cashflow = dict()
 
     for item in cap_ex:
         net_cashflow[item] = cash_from_op[item] + cap_ex[item]
-        print(net_cashflow)
-    
     return net_cashflow
 
-get_cashflow(company)
+cash = get_cashflow(company)
+

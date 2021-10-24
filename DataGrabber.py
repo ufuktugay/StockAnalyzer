777feb031a8958e23903c81bd2bdc2d6295f0ae9
Ufuk Tugay <ufukkaantugay@gmail.com>
2021-10-24 15:26:47 +0200

    added date and value converter

--- DataGrabber.py
+++ DataGrabber.py
@@ -1,21 +1,25 @@
 import yfinance as yf
-import math
 
 company = "IMBBY"
 
+def convert_values(numbers_dict):
+
+    converted_values = list()
+    for item in numbers_dict:
+        converted_values.append((item.year, item.value))
+
+    return converted_values
 
 
-'''
-Returns the net cashflow of the given ticker symbol for the last few years. Net cashflow is calculated from cash from operations and the capital expenditure.
-'''
 def get_cashflow(ticker):
+    '''Returns the net cashflow of the given ticker symbol for the last few years. Net cashflow is calculated from cash from operations and the capital expenditure.'''
     ticker = yf.Ticker(company)
     ticker_cashflow = ticker.cashflow
     ticker_cashflow= ticker_cashflow.to_dict('index')
 
     cash_from_op = ticker_cashflow["Total Cash From Operating Activities"]
     cap_ex = ticker_cashflow["Capital Expenditures"]
-    print(f"{company} Free Cashflow: ",cash_from_op)
+    #print(f"{company} Free Cashflow: ",cash_from_op)
     net_cashflow = dict()
 
     for item in cap_ex:
@@ -23,4 +27,6 @@ def get_cashflow(ticker):
     return net_cashflow
 
 cash = get_cashflow(company)
+
+print(convert_values(cash))
 
